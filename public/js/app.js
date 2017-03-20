59ecf178fa5eee20e480e8b082801febf528e12c
drilanph <drilan.ph14@gmail.com>
2017-03-20 18:11:15 +0800

    wew

--- public/js/app.js
+++ public/js/app.js
@@ -1,37 +1,3 @@
-$('#up1').html(`
-	<div class="progress">
-    <div class="indeterminate"></div>
-	</div>
-`);
-
-
-setTimeout(function(){	//**preloader to wait 0.7secs to load the inside the body
-	x();
-},777);
-
-
-function x(){
-	$.ajax({
-		url: "https://www.omdbapi.com/?t=gotham"
-
-	}).done(function(res){
-		console.log(res);
-		let html = `
-		<center>
-
-			<h1 style="font-size:2500%; font-family:Impact;">${res.Title}</h1>
-			<h3 style="font-size:300%;text-align:center;">${res.Plot}</h3>
-			<center><img src="${res.Poster}"></center>
-			<h3 style="font-size:200%;text-align:center;">${res.Genre}</h3>
-			<h3 style="font-size:200%;text-align:center;">${res.Writer}</h3>
-</center>
-
-		`;
-		$('#up1').html(html);
-	});
-	
-
-}
 
 $('#up2').html(`
 	
