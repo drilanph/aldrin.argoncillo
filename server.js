406f9c0432d8a3b242ee6fa7c85bee94b034467e
drilanph <drilan.ph14@gmail.com>
2017-03-20 17:48:32 +0800

    aldrin

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.set('port', (process.env.PORT || 5000));
 //use static files
 app.use(express.static(path.join(__dirname, 'public')));
 //express routes
-app.get('/up1', function(req, res){
+app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, 'views/up1.html'));
 });  
 app.get('/up2', function(req, res){
